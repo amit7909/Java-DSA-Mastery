c45e8dbd290e99b4680932908adb7b5087b766fb
Amit <amittiwari79099@gmail.com>
2024-12-05 14:37:46 +0530

    Rename the files and make it systematically.

--- Basics/Conditional.java
+++ Basics/Conditional.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Conditional {
     public static void main(String []args){
-        Scanner sc= new Scanner(System.in);
+        Scanner Sc= new Scanner(System.in);
 
         boolean sheAgreed = false;
         if(sheAgreed){
