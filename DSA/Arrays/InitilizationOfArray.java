35b107c2d12e74580668c6d7504e025e342f6339
Amit <amittiwari79099@gmail.com>
2025-04-21 21:05:21 +0530

    Few change

---
+++ DSA/Arrays/InitilizationOfArray.java
@@ -0,0 +1,22 @@
+public class InitilizationOfArray {
+    public static void main(String args []){
+        /* 
+        int num[] = {20,255,320,250,541,30};
+        System.out.println(num[4]);
+        */
+
+        int num[] = new int[7];
+        num[0] = 15;
+        num[1] = 20;
+        num[2] = 25;
+        num[3] = 30;
+        num[4] = 35;
+        num[5] = 40;
+        num[6] = 45;
+        System.out.println(num[6]);
+        //Printing output and Taking Input 
+        
+        //int sum[] = {10,20,30,40,50};
+
+    }
+}
